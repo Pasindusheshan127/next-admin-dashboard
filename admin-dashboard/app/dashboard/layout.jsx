41cad1953760f3713660a7854a9576312e381259
pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-10-12 19:27:18 +0530

    pages and sidebar , navbar component has benn created

---
+++ admin-dashboard/app/dashboard/layout.jsx
@@ -0,0 +1,18 @@
+import Navbar from "@/components/ui/dashboard/Navbar";
+import Sidebar from "@/components/ui/dashboard/Sidebar";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex">
+      <div className="w-[20%] bg-slate-900 p-5">
+        <Sidebar />
+      </div>
+      <div className="w-[80%] p-5">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
